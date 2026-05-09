0da4b75470f2f68b6dfa5114252e04203100ef09
acaicode <doitzyb@163.com>
2026-05-09 16:35:09 +0800

    修改布局

--- src/config.ts
+++ src/config.ts
@@ -117,7 +117,7 @@ export const NAV: readonly NavItem[] = [
   { key: 'categories', href: '/categories', icon: 'lucide:layers' },
   { key: 'tags', href: '/tags', icon: 'lucide:tag' },
   // 暂时把归档archives隐藏掉，因为没有什么实际性作用
-  // { key: 'archives', href: '/archives', icon: 'lucide:archive' },
+  { key: 'archives', href: '/archives', icon: 'lucide:archive' },
   { key: 'about', href: '/about', icon: 'lucide:info' },
 ] as const;
 
